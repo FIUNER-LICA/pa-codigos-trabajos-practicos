78a1536cabc2c731c53b063f360edf38f40534c6
DianaCarolinaVertiz <diana.vertiz@uner.edu.ar>
2024-03-03 21:09:49 -0300

    reinicio la rama main para el cursado_2024_1C

--- BibliotecaVirtual_flask/auxiliar.py
+++
@@ -1,9 +0,0 @@
-import server
-
-
-print(f"__name__ ejecutado en auxiliar.py: {__name__}")
-
-if __name__=="__main__":
-    print("Se ejecuta auxiliar.py directamente")
-else:
-    print("Se importó auxiliar desde otro módulo")
