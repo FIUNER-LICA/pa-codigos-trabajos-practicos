752caefd4a51982bfd31183006008f4313ce5107
jordaninsfran <jordan.insfran@uner.edu.ar>
2025-03-20 12:52:59 -0300

    Ejemplo de prueba de módulos e imports.

--- TrabajoPractico_1/proyecto_1/server.py
+++ TrabajoPractico_1/proyecto_1/server.py
@@ -1,6 +1,10 @@
 # Ejemplo de aplicación principal en Flask
 from flask import render_template
 from modules.config import app
+from modules.servicios import listar_nombres
+
+# from modules import servicios
+# servicios.listar_nombres()
 
 # Página de inicio
 @app.route('/')
@@ -11,6 +15,12 @@ def index():
 @app.route('/saludo')
 def funcion_saludar():
     return render_template('saludo.html')
+
+@app.route('/listar_nombres')
+def pagina_listar_nombres():
+    lista_nombres = listar_nombres("./data/nombres.txt")
+    return render_template('lista_nombres.html',
+                            nombres=lista_nombres)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
