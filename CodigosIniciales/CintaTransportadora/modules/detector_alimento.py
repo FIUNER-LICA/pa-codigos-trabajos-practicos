d580e3f94cff4165d1efb1383c23a45c4a3162e2
jordaninsfran <jordan.insfran@uner.edu.ar>
2025-05-24 15:45:35 -0300

    Se vuelven privados atributos que estában públicos.

--- CodigosIniciales/CintaTransportadora/modules/detector_alimento.py
+++ CodigosIniciales/CintaTransportadora/modules/detector_alimento.py
@@ -7,9 +7,9 @@ class DetectorAlimento:
     para detectar el tipo de alimento y su peso.
     """
     def __init__(self):
-        self.alimentos = ["kiwi", "manzana", "papa", "zanahoria", "undefined"]
-        self.peso_alimentos = np.round(np.linspace(0.05, 0.6, 12),2)
-        self.prob_pesos = np.round(self.__softmax(self.peso_alimentos)[::-1], 2)
+        self.__alimentos = ["kiwi", "manzana", "papa", "zanahoria", "undefined"]
+        self.__peso_alimentos = np.round(np.linspace(0.05, 0.6, 12),2)
+        self.__prob_pesos = np.round(self.__softmax(self.__peso_alimentos)[::-1], 2)
 
     def __softmax(self, x):
         """función softmax para crear vector de probabilidades 
@@ -21,9 +21,9 @@ class DetectorAlimento:
         """método que simula la detección del alimento y devuelve un diccionario
         con la información del tipo y el peso del alimento.
         """
-        n_alimentos = len(self.alimentos)
-        alimento_detectado = self.alimentos[random.randint(0, n_alimentos-1)]
-        peso_detectado = random.choices(self.peso_alimentos, self.prob_pesos)[0]
+        n_alimentos = len(self.__alimentos)
+        alimento_detectado = self.__alimentos[random.randint(0, n_alimentos-1)]
+        peso_detectado = random.choices(self.__peso_alimentos, self.__prob_pesos)[0]
         return {"alimento": alimento_detectado, "peso": peso_detectado}
     
 
@@ -32,8 +32,16 @@ if __name__ == "__main__":
     sensor = DetectorAlimento()
     lista_pesos = []
     
+    print("Salida de dos detecciones:")
+    print(sensor.detectar_alimento())
+    print(sensor.detectar_alimento())
+    print()
+
     for _ in range(200):
         lista_pesos.append(sensor.detectar_alimento()["peso"])
+
+    print("Primeros 10 pesos detectados:")
+    print(lista_pesos[0:10])
 
     plt.hist(lista_pesos, bins=12)
     plt.show()
