aaac998b1badc6a9e81a55c25fa23dd7764ebf72
jordaninsfran <jordan.insfran@uner.edu.ar>
2025-09-22 14:16:02 -0300

    Se añade propiedad get/set para atributo nombre (del Chef).

--- Restaurante/modules/restaurante.py
+++ Restaurante/modules/restaurante.py
@@ -1,10 +1,23 @@
 class Chef:
     def __init__(self, p_nombre, p_antiguedad = 0):
-        self.__nombre = p_nombre   # no está terminado!!!
+        # self.__nombre = p_nombre   # no está terminado!!!
+        self.nombre = p_nombre   # no está terminado!!!
         # self.__primer_nombre = p_nombre
         # self.__segundo_nombre = ''
 
         self.__antiguedad = p_antiguedad
+
+    @property
+    def nombre(self):
+        return self.__nombre
+
+    @nombre.setter
+    def nombre(self, p_nombre:str):
+        if not isinstance(p_nombre, str):
+            raise TypeError("El nombre del chef debe ser un string")
+        if p_nombre.strip() == "":
+            raise ValueError("El nombre del chef no debe estar vacío")
+        self.__nombre = p_nombre.strip()
 
     def get_nombre(self):
         return self.__nombre
@@ -31,11 +44,10 @@ class Cocina:
         self.__lista_chefs = list()
 
     def agregar_chef(self, p_chef: Chef):
-        if isinstance(p_chef, Chef):
-            self.__lista_chefs.append(p_chef)
-        else:
+        if not isinstance(p_chef, Chef):
             # Esto no: print("El chef debe ser un objeto de tipo Chef")
             raise TypeError("El chef debe ser un objeto de tipo Chef")
+        self.__lista_chefs.append(p_chef)
 
 
 class Restaurante:
@@ -49,7 +61,11 @@ class Restaurante:
             self.__chefs_contratados.append(p_chef)
         else:
             raise TypeError("El chef debe ser un objeto de tipo Chef")
-        
+
+    # rompe encapsulamiento
+    # def listar_chefs(self):
+    #     return self.__chefs_contratados
+
     def listar_chefs(self):
         res = []
         for chef in self.__chefs_contratados:
